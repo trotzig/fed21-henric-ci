cd60331b55be332d61aeb551178a2a243e1f6ef6
Henric Trotzig <henric.trotzig@gmail.com>
2020-04-14 15:39:04 +0200

    Change Hello world to actual h1
    Since that's the semantic way of doing things.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Hello world!
-        </p>
+        </h1>
       </header>
     </div>
   );
